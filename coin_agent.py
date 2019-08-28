f9a87efcfbac653b6ff99534c36b081fb8159380
srichiardi <srichiardi@L-DUB-13005630.mshome.net>
2019-08-28 17:27:24 +0100

    more modules

--- coin_agent.py
+++ coin_agent.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import datetime
+from datetime import date, datetime, timedelta
 from bisect import bisect_left
 
 
@@ -38,9 +38,13 @@ class Agent():
         self.margin = margin
         self.last_invested_price = 0
         self.last_invested_date = None
+        self.last_divested_price = 0
+        self.last_divested_date = None
         self.invested = False
+        self.agent_created = datetime.today()
+        self.first_fiat_budget = fiat_budget
 
-    def invest(self, price, date):
+    def invest(self, price, date = None):
         if self.balance.enough_funds(self.budget):
             if not self.invested:
                 # buy
@@ -48,37 +52,74 @@ class Agent():
                 self.crypt_budget = self.fiat_budget / price
                 self.fiat_budget = 0
                 self.last_invested_price = price
-                self.last_invested_date = date
+                if date:
+                    self.last_invested_date = datetime.fromisoformat(date)
+                else:
+                    self.last_invested_date = datetime.today()
+                self.invested = True
+                return True
+            else:
+                return False
+        else:
+            return False
+                    
 
-    def divest(self, price):
+    def divest(self, price, date = None):
         if self.invested:
             if ( self.last_invested_price / price - 1 ) >= self.margin:
                 # sell
                 self.fiat_budget = self.crypt_budget * price
                 self.balance.topup_balance(self.fiat_budget)
                 self.crypt_budget = 0
+                self.invested = False
+                self.last_invested_price = 0
+                self.last_divested_price = price
+                if date:
+                    self.last_divested_date = datetime.fromisoformat(date)
+                else:
+                    self.last_divested_date = datetime.today()
+                return True
+            else:
+                return False
+        else:
+            return False
+                
 
-    def report(self, action, date, price):
+    def report(self, price, date = None):
         invest_performance = ( price / self.last_invested_price - 1 ) * 100
+        if date:
+            report_date = datetime.fromisoformat(date)
+        else:
+            report_date = datetime.today()
+            
+        if self.invested:
+            invest_performance = ( price / self.last_invested_price - 1 ) * 100
+            invest_profit = (price - self.last_invested_price) * self.crypt_budget
+            
         report_data = { 'agent_name' : self.name,
-                        'action' : action,
-                        'date' : date,
+                        'date' : report_date.strftime("%Y-%m-%d"),
                         'euro_balance' : self.fiat_budget,
                         'crypt_balance' : self.crypt_budget,
-                        'last_price' : price,
-                        'investment_value' : "{0:.2f} %".format(invest_performance),
-                        'tot_value' : self.fiat_budget + ( self.crypt_budget * price ) }
+                        'curr_price' : price,
+                        'invested_price': self.last_invested_price,
+                        'investment_value_str' : "{0:.2f} %".format(invest_performance),
+                        'investment_value_pct' : invest_performance,
+                        'curr_value' : self.fiat_budget + ( self.crypt_budget * price ),
+                        'curr_profit' : invest_profit }
         return report_data
 
 
 class AgentManager():
 
-    def __init__(self, balance, range_spread = 0.2):
+    def __init__(self, balance, range_spread = 0.2, agents_logfolder = None):
         self.balance = balance
         self.spread = range_spread
         self.ranges = [0]
         self.ranges.extend(self._populate_ranges())
         self.agents = {}
+        if agents_logfolder:
+            self.log_file = agents_folder + "/Coin_Agents_Log_{}.csv".format(datetime.today().strftime("%Y%d%m-%H%M%S"))
+            
 
     def _populate_ranges(self, lower_limit = 1, upper_limit = 1000):
         next_bound = lower_limit
@@ -87,6 +128,7 @@ class AgentManager():
             next_bound *= (1 + self.spread)
             range_list.append(next_bound)
         return range_list
+    
 
     def _find_closest(self, my_number):
         """
@@ -103,7 +145,8 @@ class AgentManager():
             return self.ranges[-2]
         return self.ranges[pos - 1]
 
-    def buy_cycle(self, price):
+    
+    def buy_cycle(self, price, date = None):
         nearest_bound = self._find_closest(price)
         if nearest_bound not in self.agents.keys():
             # create a new agent
@@ -113,105 +156,31 @@ class AgentManager():
             margin = self.spread
             agent = Agent(name, fiat_budget, balance, margin)
             self.agents[nearest_bound] = agent
+        
+        investor = self.agents[nearest_bound]
+        investor.invest(price, date)
+    
+    
+    def sell_cycle(self, price, date = None):
+        for agent in self.agents.items():
+            agent.divest(price)
             
     
-
-
-class CoinAgent():
-    
-    def __init__(self,
-                 buy_price,
-                 euro_balance,
-                 crypt_balance = 0,
-                 margin = 0.2,
-                 buy = True,
-                 name = None):
-        self.buy_price = buy_price
-        self.euro_balance = euro_balance
-        self.crypt_balance = crypt_balance
-        self.profit_margin = margin
-        self.buy = buy
-        self.name = name
-        self.last_purchase_date = None
-        self.last_sale_date = None
-        self.current_price = None
-        
-    def evaluate(self, current_price, date):
-        self.current_price = current_price
-        if self.buy:
-            if current_price <= self.buy_price:
-                self.crypt_balance = self.euro_balance / current_price
-                self.euro_balance = 0
-                self.last_purchase_date = date
-                self.buy = False
-                return self.report('buy', date)
+    def status_report(self, price, date = None):
+        active_agents = 0
+        idle_agents = 0
+        tot_invested = 0
+        fiat_balance = 0
+        cryp_balance = 0
+        tot_value = 0
+        for agent in self.agents.items():
+            agent_report = agent.report(price, date)
+            if agent.invested:
+                active_agents += 1
             else:
-                return self.report('pass', date)
-        else:
-            # sell as soon as 20% profit is made
-            if current_price >= self.buy_price * ( 1 + self.profit_margin ):
-                self.euro_balance = self.crypt_balance * current_price
-                self.crypt_balance = 0
-                self.last_sale_date = date
-                self.buy = True
-                return self.report('sell', date)
-            else:
-                return self.report('pass', date)
-        
-    def report(self, action, date):
-        report_data = { 'agent_name' : self.name,
-                       'action' : action,
-                       'date' : date,
-                       'euro_balance' : self.euro_balance,
-                       'crypt_balance' : self.crypt_balance,
-                        'last_price' : self.current_price,
-                       'tot_value' : self.euro_balance + ( self.crypt_balance * self.current_price ) }
-        return report_data
+                idle_agents += 1
+            tot_invested = 0
+            fiat_balance += agent.fiat_budget
+            cryp_balance += agent.crypt_budget
+            tot_value += agent.fiat_budget + ()
 
-
-
-def invest_distr(step, agents_nr, offset, ref_price):
-    ''' this function creates a distribution of trigger prices around a price of reference 
-    step is the difference in euro between agents buy price triggers,
-    offset is a value between 0 and 1 indicating whether the ref price is the lower (0), middle (0.5) or upper (1) limit of the range,
-    ref_price is a value that should be set to a sort of average of volatility.'''
-    start = step
-    stop = step + step * agents_nr
-    actual_offset = ref_price - ( stop ) * offset
-    return [ x + actual_offset for x in range(start, stop, step) ]
-
-    
-        
-def main():
-    historical_records = "../ETH.csv"
-    report_file = "../results.csv"
-    euro_balance = 1000
-    agents_number = 10
-    current_price = 10
-    agent_balance = euro_balance / agents_number
-    buy_price_distr = invest_distr(100, agents_number, 0, current_price)
-    agents_box = []
-    for i in range(agents_number):
-        agent_name = "agent_{}".format(i)
-        agent = CoinAgent(buy_price_distr[i],
-                          agent_balance,
-                          name = agent_name)
-        agents_box.append(agent)
-        
-    with open(historical_records, newline='') as ftr:
-        file_reader = csv.DictReader(ftr)
-        with open(report_file, 'w', newline='') as ftw:
-            field_names = ['agent_name', 'action', 'date', 'euro_balance', 'crypt_balance', 'last_price', 'tot_value']
-            file_writer = csv.DictWriter(ftw, fieldnames=field_names)
-            file_writer.writeheader()
-            for row in file_reader:
-                today_price = float(row['Close'].replace(",", ""))
-                today_date = row['DATE']
-                for agent in agents_box:
-                    results = agent.evaluate(today_price, today_date)
-                    if results['action'] != "pass":
-                        file_writer.writerow(results)
-            # once loop is done, print all agents' report
-            for agent in agents_box:
-                results = agent.report('exit', today_date)
-                file_writer.writerow(results)
